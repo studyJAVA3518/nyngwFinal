d0f081b5a20cab2b202233467c9d4872adf9c81d
syChoi <minamcra@hotmail.com>
2017-07-17 15:09:00 +0900

    정보공유>직원정보 완료!!

---
+++ nyngw_final/src/main/java/com/nyngw/dto/AddressBookVO.java
@@ -0,0 +1,55 @@
+package com.nyngw.dto;
+
+public class AddressBookVO {
+	private String mem_dept_name;
+	private String mem_name;
+	private String mem_position_name;
+	private String mem_email;
+	private String mem_tel;
+	private String mem_addr1;
+	private String mem_dept_number;
+
+	
+	public String getMem_dept_number() {
+		return mem_dept_number;
+	}
+	public void setMem_dept_number(String mem_dept_number) {
+		this.mem_dept_number = mem_dept_number;
+	}
+	public String getMem_dept_name() {
+		return mem_dept_name;
+	}
+	public void setMem_dept_name(String mem_dept_name) {
+		this.mem_dept_name = mem_dept_name;
+	}
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public String getMem_position_name() {
+		return mem_position_name;
+	}
+	public void setMem_position_name(String mem_position_name) {
+		this.mem_position_name = mem_position_name;
+	}
+	public String getMem_email() {
+		return mem_email;
+	}
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	public String getMem_tel() {
+		return mem_tel;
+	}
+	public void setMem_tel(String mem_tel) {
+		this.mem_tel = mem_tel;
+	}
+	public String getMem_addr1() {
+		return mem_addr1;
+	}
+	public void setMem_addr1(String mem_addr1) {
+		this.mem_addr1 = mem_addr1;
+	}
+}
