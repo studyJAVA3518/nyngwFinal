722562258c12ae1181e80d6d220467ffa8aa7ef9
syChoi <minamcra@hotmail.com>
2017-07-18 14:01:36 +0900

    정보공유>직원정보>주소록 ((페이징처리 '다음' '이전' 나오게))

--- nyngw_final/src/main/java/com/nyngw/sharingInformation/memberInformation/service/MemberInformationServiceImpl.java
+++ nyngw_final/src/main/java/com/nyngw/sharingInformation/memberInformation/service/MemberInformationServiceImpl.java
@@ -23,9 +23,11 @@ public class MemberInformationServiceImpl implements MemberInformationService {
 	@Autowired
 	private MemberInformationDaoImpl memberInformationDao;
 
-	private static final int ADDRESSBOOK_COUNT_PER_PAGE = 20;
-	private static final int BIRTHDAY_COUNT_PER_PAGE = 20;
+	private static final int ADDRESSBOOK_COUNT_PER_PAGE = 5;	//한 주소록에 보여줄 주소록 수
+	private static final int BIRTHDAY_COUNT_PER_PAGE = 20;		//한 생일정보에 보여줄 생일자 수
+	private static final int PAGE_NUMBER_COUNT_PER_PAGE = 5;			//한 페이지에 보여줄 페이지 번호 수
 	
+	//조직도를 출력하기 위해 모든 부서를 검색해 맵 형식으로 (Json데이터 형식)으로 변환하여 리턴해주는 메서드
 	public List<Map> getAllDepartment() {
 		List<DepartmentVO> departmentList = memberInformationDao.SI_selectDepartment();
 
@@ -42,42 +44,58 @@ public class MemberInformationServiceImpl implements MemberInformationService {
 		return treeJsonList;
 	}
 
+	//주소록 정보를 가져오기 위해 DB에 접속하여 검색하고 model에 담아주는 메서드
 	public void getMemberInfo(int pageNumber,Model model,String mem_name) {
+		//처리에 필요한 것들 준비
 		List<AddressBookVO> addressBookList = new ArrayList<AddressBookVO>();
 		int currentPageNumber = pageNumber;
 		int memberTotalCount = 0;
-		if(mem_name.equals("")){
-			memberTotalCount = memberInformationDao.SI_selectAllMemberCount();
-		}else{
-			memberTotalCount = memberInformationDao.SI_selectMemberCount(mem_name);
-		}
 		int firstRow = 0;
 		int endRow = 0;
-		if (memberTotalCount > 0) {
+		//사원의 수(count)를 검색 
+		if(mem_name.equals("")){	//모든 사원의 수
+			memberTotalCount = memberInformationDao.SI_selectAllMemberCount();
+		}else{						//검색한 이름을 가진 사원의 수
+			memberTotalCount = memberInformationDao.SI_selectMemberCount(mem_name);
+		}
+		
+		//위에서 구한 숫자로 주소록 검색 실행
+		if (memberTotalCount > 0) {	//만약 검색한 사원의 정보고 DB에 있으면
+			//////페이지 번호에 따라 필요한 갯수만큼만 검색하기 위해 rowBounds를 설정해주는 곳//////
 			firstRow = (pageNumber - 1) * ADDRESSBOOK_COUNT_PER_PAGE + 1;
 			endRow = firstRow + ADDRESSBOOK_COUNT_PER_PAGE - 1;
 			int offset = firstRow - 1;
 			int limit = endRow - firstRow + 1;
 			RowBounds rowBounds = new RowBounds(offset, limit);
-			addressBookList = memberInformationDao.SI_selectAllMember(mem_name, rowBounds);
-			/*if(select.getVal()!=null && !select.getVal().equals("")){
-				MemberTotalCount = memberInformationDao.SI_MemberSelectCount(select);
-			}	검색할 때 사용할 부분*/
-		} else {
+			//////////////////////////////////////////////////////////////////
+			
+			addressBookList = memberInformationDao.SI_selectAllMember(mem_name, rowBounds);	//rowBounds를 가지고 회원을 검색
+		} else {	//만약 검색한 사원이 없으면
 			currentPageNumber = 0;
 			addressBookList = Collections.emptyList();
 		}
-		model.addAttribute("addressBookViewVO", new AddressBookViewVO(addressBookList, memberTotalCount,
-				currentPageNumber, ADDRESSBOOK_COUNT_PER_PAGE, firstRow, endRow));
+		
+		//위에서 구한 값들을 View에 설정해준 후 model에 담아 보낸다.
+		AddressBookViewVO addressBookViewVO = new AddressBookViewVO(addressBookList, memberTotalCount,
+				currentPageNumber, ADDRESSBOOK_COUNT_PER_PAGE, firstRow, endRow);
+		model.addAttribute("addressBookViewVO", addressBookViewVO);
+		
+		//페이지 번호별로 이전 다음 설정해주기 위해 설정
+		if(addressBookViewVO.getPageTotalCount()>0){
+			int beginPageNumber = (addressBookViewVO.getCurrentPageNumber()-1)/PAGE_NUMBER_COUNT_PER_PAGE*PAGE_NUMBER_COUNT_PER_PAGE+1;
+			int endPageNumber = beginPageNumber+ PAGE_NUMBER_COUNT_PER_PAGE-1;
+			if(endPageNumber > addressBookViewVO.getPageTotalCount()){
+				endPageNumber = addressBookViewVO.getPageTotalCount();
+			}
+			model.addAttribute("perPage", PAGE_NUMBER_COUNT_PER_PAGE);	//페이지 번호의 갯수
+			model.addAttribute("end", addressBookViewVO.getAddressBookList().size()-1);//마지막 페이지
+			model.addAttribute("beginPage", beginPageNumber);	//보여줄 페이지 번호의 시작
+			model.addAttribute("endPage", endPageNumber);		//보여줄 페이지 번호의 끝
+		}
+		
+		//검색어를 넘겨주기 위한 model 셋팅
+		model.addAttribute("mem_name",mem_name);
 	}
-	
-	private List<BirthdayVO> birthdayList;
-	private int memberTotalCount;
-	private int currentPageNumber;
-	private int pageTotalCount;
-	private int birthdayCountPerPage;
-	private int firstRow;
-	private int endRow;
 	
 	public void getBirthdayMember(int pageNumber,Model model,String month) {
 		month = (Integer.parseInt(month) < 10 ? "0" : "") + month;
