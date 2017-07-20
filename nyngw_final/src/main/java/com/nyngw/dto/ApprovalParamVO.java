0a476613ffc2fa3f347fc4698da140cecf1654e6
syChoi <minamcra@hotmail.com>
2017-07-20 20:57:35 +0900

    기안하기 끝!

---
+++ nyngw_final/src/main/java/com/nyngw/dto/ApprovalParamVO.java
@@ -0,0 +1,112 @@
+package com.nyngw.dto;
+
+public class ApprovalParamVO {
+	private String param_ea_startdate;
+	private String param_ea_enddate;
+	private String param_ea_writedate;
+	private String approvalMember1;
+	private String approvalMember2;
+	private String approvalMember3;
+	private String approvalMember4;
+	private String approvalMember5;
+	private String agreementMember1;
+	private String agreementMember2;
+	private String agreementMember3;
+	private String agreementMember4;
+	private String agreementMember5;
+	private String implementMembers;
+	private String referenceMembers;
+	
+	public String getParam_ea_startdate() {
+		return param_ea_startdate;
+	}
+	public void setParam_ea_startdate(String param_ea_startdate) {
+		this.param_ea_startdate = param_ea_startdate;
+	}
+	public String getParam_ea_enddate() {
+		return param_ea_enddate;
+	}
+	public void setParam_ea_enddate(String param_ea_enddate) {
+		this.param_ea_enddate = param_ea_enddate;
+	}
+	public String getParam_ea_writedate() {
+		return param_ea_writedate;
+	}
+	public void setParam_ea_writedate(String param_ea_writedate) {
+		this.param_ea_writedate = param_ea_writedate;
+	}
+	public String getApprovalMember1() {
+		return approvalMember1;
+	}
+	public void setApprovalMember1(String approvalMember1) {
+		this.approvalMember1 = approvalMember1;
+	}
+	public String getApprovalMember2() {
+		return approvalMember2;
+	}
+	public void setApprovalMember2(String approvalMember2) {
+		this.approvalMember2 = approvalMember2;
+	}
+	public String getApprovalMember3() {
+		return approvalMember3;
+	}
+	public void setApprovalMember3(String approvalMember3) {
+		this.approvalMember3 = approvalMember3;
+	}
+	public String getApprovalMember4() {
+		return approvalMember4;
+	}
+	public void setApprovalMember4(String approvalMember4) {
+		this.approvalMember4 = approvalMember4;
+	}
+	public String getApprovalMember5() {
+		return approvalMember5;
+	}
+	public void setApprovalMember5(String approvalMember5) {
+		this.approvalMember5 = approvalMember5;
+	}
+	public String getAgreementMember1() {
+		return agreementMember1;
+	}
+	public void setAgreementMember1(String agreementMember1) {
+		this.agreementMember1 = agreementMember1;
+	}
+	public String getAgreementMember2() {
+		return agreementMember2;
+	}
+	public void setAgreementMember2(String agreementMember2) {
+		this.agreementMember2 = agreementMember2;
+	}
+	public String getAgreementMember3() {
+		return agreementMember3;
+	}
+	public void setAgreementMember3(String agreementMember3) {
+		this.agreementMember3 = agreementMember3;
+	}
+	public String getAgreementMember4() {
+		return agreementMember4;
+	}
+	public void setAgreementMember4(String agreementMember4) {
+		this.agreementMember4 = agreementMember4;
+	}
+	public String getAgreementMember5() {
+		return agreementMember5;
+	}
+	public void setAgreementMember5(String agreementMember5) {
+		this.agreementMember5 = agreementMember5;
+	}
+	public String getImplementMembers() {
+		return implementMembers;
+	}
+	public void setImplementMembers(String implementMembers) {
+		this.implementMembers = implementMembers;
+	}
+	public String getReferenceMembers() {
+		return referenceMembers;
+	}
+	public void setReferenceMembers(String referenceMembers) {
+		this.referenceMembers = referenceMembers;
+	}
+	
+	
+}
